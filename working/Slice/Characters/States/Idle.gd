3bfbb095dbda3a05360ebdb440a3500b27516a5c
Justin Wash <justin.wash@pm.me>
2019-11-06 02:07:31 -0600

    formatting

--- working/Slice/Characters/States/Idle.gd
+++ working/Slice/Characters/States/Idle.gd
@@ -3,9 +3,11 @@ extends Node2D
 func update_state(player):
 	if (Input.is_action_pressed("player_up")):
 		player.current_state = player.states["jump"]
+
 	elif (Input.is_action_pressed("player_right")
 	or  Input.is_action_pressed("player_left")):
 		player.current_state = player.states["walk"]
+
 	else:
 		player.move_and_slide(Vector2(0, player.stats.GRAVITY), Vector2(0,-1))
 
--- working/Slice/Characters/States/Jump.gd
+++ working/Slice/Characters/States/Jump.gd
@@ -5,6 +5,7 @@ var jump_direction_delta
 
 func ready_state(player):
 	jump_velocity_delta = player.stats.JUMP_VELOCITY
+
 	if   Input.is_action_pressed("player_right"):
 		jump_direction_delta = 1
 	elif Input.is_action_pressed("player_left"):
@@ -13,7 +14,6 @@ func ready_state(player):
 		jump_direction_delta = 0
 
 func update_state(player):
-
 	player.move_and_slide(Vector2(player.stats.MOVE_SPEED * jump_direction_delta * player.stats.JUMP_X_MODIFIER,
 		player.stats.GRAVITY - jump_velocity_delta), Vector2(0,-1))
 
