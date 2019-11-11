8c2c288c595f1e098402cc0a80ed4d3115543bb7
Justin Wash <justin.wash@pm.me>
2019-11-11 11:51:17 -0600

    implement pause menu

--- HYPERVIZOR/Slice/Menus/Pause.gd
+++ HYPERVIZOR/Slice/Menus/Pause.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("pause"):
-		get_tree().paused = true
+		get_tree().paused = !get_tree().paused
+		$MenuTitle.visible = ! $MenuTitle.visible
