51c21eec378a43bcce1f57f787071fe3b91d53d9
Justin Wash <justin.wash@pm.me>
2020-03-26 17:50:45 -0500

    interaction icno scene

--- HYPERVIZOR/Slice/Interactables/Terminals/ComputerTerminal.gd
+++ HYPERVIZOR/Slice/Interactables/Terminals/ComputerTerminal.gd
@@ -8,18 +8,25 @@ var is_open = false
 func _ready():
 	anim_player = $AnimationPlayer
 	
-func activate():
-	if halted:
-		return
-		
-	if is_open == false:
-		$Window/TextLog.visible = true
-		$Window/Border.visible = true
-		is_open = true
-		
+func on_enter(player):
+	$Icon.visible = true
+
 func on_exit(player):
 	if halted:
 		return
 	$Window/TextLog.visible = false
 	$Window/Border.visible = false
 	is_open = false
+	$Icon.activated_t = 0
+	$Icon.visible = false
+	
+func activate():
+	if halted:
+		return
+	
+	$Icon.activate()
+		
+	if is_open == false:
+		$Window/TextLog.visible = true
+		$Window/Border.visible = true
+		is_open = true
